29d94f10db23905e17329e4758aec033b8b2e558
PaladiAlexandru <32801931+PaladiAlexandru@users.noreply.github.com>
2022-06-06 12:44:50 +0300

    Buttons refactor

--- react-redux-hooks-jwt-auth-master/src/components/UserCourse.js
+++ react-redux-hooks-jwt-auth-master/src/components/UserCourse.js
@@ -3,9 +3,9 @@ import React from "react";
 const UserCourse = (props) =>{
     var {courseName, owned, handleOnClick, id} = props;
 
-    return ( <p><span className={`d-flex p-4 bg-info text-white font-weight-bold rounded`}>{courseName}                         
-    {owned ? (<button type="button" className="btn btn-danger" onClick={(e) => handleOnClick(e)} id={id}>Leave</button>):
-    (<button type="button" className="btn btn-primary " onClick={(e) => handleOnClick(e)} id={id}>Join</button>)}
+    return ( <p><span className={`d-flex p-4 bg-info text-white font-weight-bold rounded`} style={{position: 'relative'}}>{courseName}                         
+    {owned ? (<button type="button" className="btn btn-danger" onClick={(e) => handleOnClick(e)} id={id} style={{position: 'absolute',right:'5%'}}>Leave</button>):
+    (<button type="button" className="btn btn-primary " onClick={(e) => handleOnClick(e)} id={id} style={{position: 'absolute',right:'5%'}}>Join</button>)}
     </span></p>
   );
 
